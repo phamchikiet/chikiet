bd1679c333c6523ca9fbd66bfc5d08a6eba57267
quocbao1983 <quocbao280783@gmail.com>
2023-12-20 23:43:44 +0700

    uypdate

--- fe_ketoan/src/environments/environment.ts
+++ fe_ketoan/src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   APIURL:'http://localhost:3100',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
-  zalo_redirect_uri:"http://localhost:4200/admin/zalotoken"
+  zalo_redirect_uri:"http://localhost:4200/admin/zalotoken",
+  Token : "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1OTAwNDI4OTA0IiwidHlwZSI6MiwiZXhwIjoxNzAyOTkzOTIwLCJpYXQiOjE3MDI5MDc1MjB9.LsoDY00iPix6tCFWLHCIGz8BmbH610viZoYe29rIQeWDhNencZSbDHrxyS6MILfLl91K11m-0qb-cWkiJMh7Cw"
     //APIURL:'https://api.tazaskinclinic.com',
 };
