d2a1e5090c129a56c79350f1dc0d1a00d685ca11
chikiet88 <chikiet88@gmail.com>
2024-01-20 08:40:59 +0700

    update

--- fe_shop/src/environments/environment.ts
+++ fe_shop/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production:false,
   APITINYMCE: "1cdi3qs7qw7nogvpu6poxqc6z7bf4a4hurwyao0kdbd741dl",
-  APIURL:'http://localhost:3200',
+  APIURL:'https://api.hderma.vn',
+  // APIURL:'http://localhost:3200',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
   zalo_redirect_uri:"http://localhost:4200/admin/zalotoken"
