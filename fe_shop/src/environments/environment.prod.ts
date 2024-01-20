7bad9cbf28fc16c8a29bcdb416d7e5f89fd27d98
chikiet88 <chikiet88@gmail.com>
2024-01-20 11:22:05 +0700

    update

--- fe_shop/src/environments/environment.prod.ts
+++ fe_shop/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:true,
   APITINYMCE: "1cdi3qs7qw7nogvpu6poxqc6z7bf4a4hurwyao0kdbd741dl",
-  APIURL:'http://apishop.chikiet.com',
+  APIURL:'https://apishop.chikiet.com',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
   zalo_redirect_uri:"https://shop.chikiet.com/admin/zalotoken"
