5e97a12a26af5a50eea46f45775c7dc69aefd648
quocbao1983 <quocbao280783@gmail.com>
2024-02-15 12:24:27 +0700

    update

--- apps/beketoan/src/main.ts
+++ apps/beketoan/src/main.ts
@@ -10,7 +10,8 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3333;
+  app.enableCors();
+  const port = process.env.PORT || 3100;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}`
