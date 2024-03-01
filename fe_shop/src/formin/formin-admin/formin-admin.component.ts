4110bafb3d1170af2574a4397d3cfce67923b18e
quocbao1983 <quocbao280783@gmail.com>
2024-03-01 22:22:01 +0700

    update

--- fe_shop/src/formin/formin-admin/formin-admin.component.ts
+++ fe_shop/src/formin/formin-admin/formin-admin.component.ts
@@ -10,12 +10,14 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ForminAdminComponent implements OnInit {
   @Input() Donhang:any ={}
+  @Input() Tongthucte:any =0
   @Input() Taikhoan:any ={STK:'9199217',TenTK:"TRAN HUU LANH",TenNH:"Ngân hàng TMCP Á Châu (ACB)"}
   
   constructor() { }
 
   ngOnInit() {
     console.log(this.Donhang);
+    console.log(this.Tongthucte);
     
   }
 
