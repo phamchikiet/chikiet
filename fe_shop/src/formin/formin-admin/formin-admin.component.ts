e7656b6c9d1c7830413e713dff1b191996e01e6d
chikiet88 <chikiet88@gmail.com>
2024-03-08 08:47:46 +0700

    update

--- fe_shop/src/formin/formin-admin/formin-admin.component.ts
+++ fe_shop/src/formin/formin-admin/formin-admin.component.ts
@@ -49,7 +49,7 @@ export class ForminAdminComponent implements OnInit {
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
       const contentDataURL = canvas.toDataURL('image/png')
-      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      let pdf = new jspdf('p', 'mm', 'a5'); // A4 size page of PDF
       var position = 0;
       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
       pdf.save(`${this.Donhang.MaDonHang}_${(new Date()).getTime()}.pdf`); // Generated PDF
