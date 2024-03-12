b3bc596697842b03a0413876cde54c0470147873
chikiet88 <chikiet88@gmail.com>
2024-03-12 09:58:00 +0700

    update

--- fe_shop/src/environments/environment.prod.ts
+++ fe_shop/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:true,
   APITINYMCE: "1cdi3qs7qw7nogvpu6poxqc6z7bf4a4hurwyao0kdbd741dl",
-  pathServer:"/home/jtnkwfpz/public_html/shop/images",
+  pathServer:"/home/zbhykhog/public_html/shop/images",
   APIURL:'https://api.rausachtrangia.com',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
