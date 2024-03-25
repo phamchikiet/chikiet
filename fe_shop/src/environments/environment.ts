23866563056dd678fc514e7457820f7ced2a63b6
chikiet88 <chikiet88@gmail.com>
2024-03-25 13:36:39 +0700

    update

--- fe_shop/src/environments/environment.ts
+++ fe_shop/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   APITINYMCE: "1cdi3qs7qw7nogvpu6poxqc6z7bf4a4hurwyao0kdbd741dl",
   pathServer:"/home/jtnkwfpz/public_html/shop/images",
   //APIURL:'http://localhost:3200',
-  APIURL:'https://apishop.chikiet.com',
+  APIURL:'http://api.rausachtrangia.com',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
   zalo_redirect_uri:"http://localhost:4200/admin/zalotoken",
