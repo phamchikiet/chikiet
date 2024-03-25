f90f5b9810fa3b001b28baeec1eb44047af70c80
chikiet88 <chikiet88@gmail.com>
2024-03-25 13:40:50 +0700

    update

--- fe_shop/src/environments/environment.ts
+++ fe_shop/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   APITINYMCE: "1cdi3qs7qw7nogvpu6poxqc6z7bf4a4hurwyao0kdbd741dl",
   pathServer:"/home/zbhykhog/public_html/shop/images",
   //APIURL:'http://localhost:3200',
-  APIURL:'http://api.rausachtrangia.com',
+  APIURL:'https://api.rausachtrangia.com',
   app_id:"1416835846626859002",
   secret_key:"HLBWj23hToA9fuiQvQY4",
   zalo_redirect_uri:"http://localhost:4200/admin/zalotoken",
